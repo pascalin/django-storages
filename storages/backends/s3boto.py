6d22979e5f9284d48f22a6c85c361fff95ba6fe8
David Suarez <david.suarez@yahoo.com>
2013-12-27 12:56:59 -0600

    Added config key AWS_S3_HOST to enable other S3 compatible services like DreamHost's DreamObjects.

--- storages/backends/s3boto.py
+++ storages/backends/s3boto.py
@@ -210,6 +210,7 @@ class S3BotoStorage(Storage):
 
     access_key = setting('AWS_S3_ACCESS_KEY_ID', setting('AWS_ACCESS_KEY_ID'))
     secret_key = setting('AWS_S3_SECRET_ACCESS_KEY', setting('AWS_SECRET_ACCESS_KEY'))
+    host = setting('AWS_S3_HOST', 's3.amazonaws.com')
     file_overwrite = setting('AWS_S3_FILE_OVERWRITE', True)
     headers = setting('AWS_HEADERS', {})
     bucket_name = setting('AWS_STORAGE_BUCKET_NAME')
@@ -266,7 +267,8 @@ class S3BotoStorage(Storage):
         if self._connection is None:
             self._connection = self.connection_class(
                 self.access_key, self.secret_key,
-                calling_format=self.calling_format)
+                calling_format=self.calling_format,
+                host=self.host)
         return self._connection
 
     @property
